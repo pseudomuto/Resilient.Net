708fc9429288a1df0d2cbe34c95efbb8b7f71372
pseudomuto <david.muto@gmail.com>
2016-02-01 16:19:23 -0500

    Adding shared assembly info to SolutionInfo.cs

---
+++ SolutionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("pseudomuto")]
+[assembly: AssemblyProduct("Resilient.Net")]
+[assembly: AssemblyCopyright("Copyright © pseudomuto 2016")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+
+[assembly: ComVisible(false)]
